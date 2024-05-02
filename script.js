0a334c4e4f1e8a82c620577632c29eef8bdba401
ABHIRAJ9064 <159516528+ABHIRAJ9064@users.noreply.github.com>
2024-05-02 21:31:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,12 @@
-function Employee(name, salary) {
-  this.name = name;
-  this.salary = salary;
-}
 
-Employee.prototype.giveRaise = function(amount) {
-  this.salary += amount;
-}
+function Employee(name, salary) {}
 
-function Manager(name, salary, department) {
-  Employee.call(this, name, salary);
-  this.department = department;
-}
+Employee.prototype.giveRaise = function (amount) {};
 
-Manager.prototype = Object.create(Employee.prototype);
-Manager.prototype.constructor = Manager;
+function Manager(name, salary, department) {}
 
-Manager.prototype.giveRaise = function(amount) {
-  Employee.prototype.giveRaise.call(this, amount);
-  console.log(`New salary for ${this.name} in ${this.department}: ${this.salary}`);
-}
+Manager.prototype.giveRaise = function (amount) {};
+
+// do not change the code below this
+window.Employee = Employee;
+window.Manager = Manager;
